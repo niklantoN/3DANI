5701b248578c21b0ed05c7eb12f613fd59b43fbf
niklashopeasaari <aa3047@student.jamk.fi>
2025-02-11 09:19:42 +0200

    basic movement

---
+++ 3dani/Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,47 @@
+using Newtonsoft.Json.Bson;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraHandler : MonoBehaviour
+{
+    private Vector2 _input;
+    private CameraRotation _cameraRotation;
+
+    [SerializeField] private GameObject _camera;
+    [SerializeField] private float _cameraSensivity;
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _distanceToTarget;
+    [SerializeField] private float _cameraHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _camera.transform.eulerAngles = new Vector3(
+            _camera.transform.eulerAngles.x + _input.y * Time.deltaTime * _cameraSensivity,
+            _camera.transform.eulerAngles.y + _input.x * Time.deltaTime * _cameraSensivity,
+            _camera.transform.eulerAngles.z
+            );
+
+        _camera.transform.position = _target.position - transform.forward * _distanceToTarget + transform.up * _cameraHeight;
+
+    }
+
+    public void Look(InputAction.CallbackContext context)
+    {
+        _input = context.ReadValue<Vector2>();
+    }
+}
+
+public struct CameraRotation
+{
+    public float Pitch;
+    public float Yaw;
+}
---
+++ 3dani/Assets/Scripts/PlayerHandler.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerHandler : MonoBehaviour
+{
+    private Vector2 _input;
+    private CharacterController _characterController;
+    private Vector3 _direction;
+    private float _velocity;
+    private bool _isGrounded;
+
+    private LayerMask _groundLayer;
+
+    [SerializeField] private float _maxDistance;
+    [SerializeField] private float _movementSpeed;
+    [SerializeField] private float _gravity;
+    [SerializeField] private float _gravityMultiplier;
+    [SerializeField] private float _jumpStrength;
+    [SerializeField] private Transform _camera;
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        _isGrounded = _characterController.isGrounded;
+
+
+
+        ApplyGravity();
+        RotateCharacter();
+        MoveCharacter();
+    }
+
+
+
+
+
+
+
+
+    private void RotateCharacter()
+    {
+        if (_input.sqrMagnitude == 0) return;
+        
+            var targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, targetAngle + _camera.eulerAngles.y, 0);
+        
+    }
+
+    private void MoveCharacter()
+    {
+        Vector3 movementInCameraSpace = ConvertToCameraSpace(_direction);
+        _characterController.Move(movementInCameraSpace * Time.deltaTime * _movementSpeed);
+    }
+
+    private void ApplyGravity()
+    {
+        if (_isGrounded && _velocity < 0.0f)
+        {
+            _velocity = -1;
+        }
+        else
+        {
+            _velocity += _gravity * _gravityMultiplier * Time.deltaTime;
+        }
+        _direction.y = _velocity;
+    }
+
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+
+            if (!context.started) return;
+            if(!_isGrounded) return;
+            _velocity += _jumpStrength;
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        _input = context.ReadValue<Vector2>();
+        if (_input.magnitude > 1)
+        {
+            _input.Normalize();
+        }
+        _direction = new Vector3(_input.x, 0, _input.y);
+    }
+
+    Vector3 ConvertToCameraSpace(Vector3 direction)
+    {
+        Quaternion rotation = Quaternion.Euler(0, _camera.eulerAngles.y, 0);
+
+        Vector3 rotatedVector = rotation * direction;
+        return rotatedVector;
+    }
+
+}
